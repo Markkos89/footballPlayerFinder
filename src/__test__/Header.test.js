9acde90dd8dc372b2983ae7976197846085283f9
Marcos Iglesias <markkos89@gmail.com>
2019-04-12 18:09:22 -0300

    testing header

---
+++ src/__test__/Header.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../footballPlayerFinder/components/Header.js';
+
+describe('Header component', () => {
+
+	it("it should render without errors", () => {
+		expect(1).toBe(1);
+	});
+});
